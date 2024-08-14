d82a3a83c073348114ed580549495644d22c570f
YurifromSiberia <mypost2000@yahoo.com>
2024-08-14 11:20:27 +0700

    added slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,13 @@
 $(function() {
 
 
-    
+    $('.slider__inner').slick({
+        slidesToShow: 1,
+        sliderToScrol: 1,
+        speed:1000,
+        prevArrow:   '<button class="slick-arrow slick-prev"><img src="img/icons/chevron-left.png" alt=""></button> ' ,
+        nextArrow:  '<button class="slick-arrow slick-next"><img  src="img/icons/chevron-right.png" alt=""></button> ' 
+       });
 
 
 
